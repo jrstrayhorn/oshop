7fe1deb781a60094f4cf764f26a1266bfd4432e7
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-08-19 10:26:52 -0400

    done with product management, building a bootstrap form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
 import { UserService } from './user.service';
 import { AdminAuthGuard } from './admin-auth-guard.service';
+import { ProductFormComponent } from './admin/product-form/product-form.component';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { AdminAuthGuard } from './admin-auth-guard.service';
     MyOrdersComponent,
     AdminProductsComponent,
     AdminOrdersComponent,
-    LoginComponent
+    LoginComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule,
@@ -56,7 +58,13 @@ import { AdminAuthGuard } from './admin-auth-guard.service';
       { 
         path: 'admin/products', 
         component: AdminProductsComponent, 
-        canActivate: [AuthGuard, AdminAuthGuard] },  // in future, define admin module and move admin related components to that module then dont need to prefix
+        canActivate: [AuthGuard, AdminAuthGuard] 
+      },
+      { 
+        path: 'admin/products/new', 
+        component: ProductFormComponent, 
+        canActivate: [AuthGuard, AdminAuthGuard] 
+      },
       { 
         path: 'admin/orders', 
         component: AdminOrdersComponent, 
