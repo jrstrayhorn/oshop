dc75975e69996526b85633b9054cc21a39cbfda4
J.R. Strayhorn <jstrayhorn@gmail.com>
2018-09-17 09:31:56 -0400

    wip product catalog

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -7,10 +10,33 @@ import { ProductService } from '../product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  categories$;
+  category: string;
 
-  constructor(productService: ProductService) { 
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService, 
+    categoryService: CategoryService) { 
+
+    productService.getAll().subscribe(products => {
+      this.products = products;
+
+      // cant use snapshot because component is not being destroyed
+      route.queryParamMap.subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ? 
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+      });
+      
+    });
+    
+    this.categories$ = categoryService.getAll();
+
+    
   }
 
 
