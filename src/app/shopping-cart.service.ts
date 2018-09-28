e8715e46eabdd86258a470416a465c27554d6f99
J.R. Strayhorn <jrstrayhorn@gmail.com>
2018-09-28 09:27:23 -0400

    done with adding a product to shopping cart

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Product } from './models/product';
+
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,15 +21,26 @@ export class ShoppingCartService {
     return this.db.object(`/shopping-carts/${cartId}`).valueChanges();
   }
 
-  private async getOrCreateCart() {
+  private async getOrCreateCartId() {
     let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      let result = await this.create();
-      localStorage.setItem('cartId', result.key);
-      return this.getCart(result.key);
-    } 
+    if (cartId) return cartId;
 
-    return this.getCart(cartId);
-    
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+
+  async addToCart(product: Product) {
+    let cartId = await this.getOrCreateCartId();
+    let item$ = this.db.object(`/shopping-carts/${cartId}/items/${product.key}`);
+    item$
+      .snapshotChanges()
+      .pipe(
+        take(1)
+        )
+      .subscribe(snapshot => {
+        if (snapshot.payload.exists()) item$.update({ quantity: snapshot.payload.val()['quantity'] + 1});
+        else item$.set({ product: product, quantity: 1 });
+      })
   }
 }
