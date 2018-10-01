42712d861800faaa7d5bd31f0f8123c91eec9f62
J.R. Strayhorn <jrstrayhorn@gmail.com>
2018-10-01 09:35:16 -0400

    done with displaying the quantity

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,23 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import { switchMap } from 'rxjs/operators';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
   filteredProducts: Product[] = [];
   category: string;
+  cart: any;
+  subscription: Subscription;
 
   constructor(
     route: ActivatedRoute,
     productService: ProductService, 
+    private shoppingCartService: ShoppingCartService
     ) { 
+    
+    
 
     productService.getAll()
       .pipe(
@@ -33,5 +40,16 @@ export class ProductsComponent {
           this.products.filter(p => p.category === this.category) :
           this.products;
       });
+  }
+
+  // using ngOnInit because we need to call async method
+  // and cant call async method in a constructor so use ngOnInit
+  async ngOnInit() {
+    this.subscription = (await this.shoppingCartService.getCart())
+      .subscribe(cart => this.cart = cart)
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
